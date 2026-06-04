d0c2e59883f52ab23067bac1f153a1af3dfb0c1a
davidbabs50-blip <davidbabs50@gmail.com>
2026-06-04 15:15:26 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Tobi Bot",
   botOwner: 'Tobi', // Your name
   ownerNumber: '2349166836588', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
